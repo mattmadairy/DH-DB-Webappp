e5f91f74a41d701a60ece27bfc3fb9958e0b2753
Matthew Madairy <mattmadairy@gmail.com>
2025-11-23 00:46:19 -0500

    m

--- DH_DB_Webapp/add_deleted_on_column.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('members.db')
-c = conn.cursor()
-c.execute("ALTER TABLE members ADD COLUMN deleted_on TEXT;")
-conn.commit()
-conn.close()
-print("Column 'deleted_on' added to members table.")
