b8ed7e4ad49f87d5587dcd3ca2fe09621f9bb6b4
Matthew Madairy <mattmadairy@gmail.com>
2026-01-06 17:33:12 -0500

    refactor: update kiosk layout for improved responsiveness and remove logo section

--- DH_DB_Webapp/app.py
+++ DH_DB_Webapp/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
